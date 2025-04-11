07f75a8621d8d503592fa101afe82ad0bcce4ea9
averyiaa <aaveryia@gmail.com>
2025-04-12 01:06:49 +0200

    add keybinds to toggle nvim-tree focus

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,5 +4,9 @@ vim.g.mapleader = " "
 -- yank to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 
--- keybind to toggle the tree
+-- toggle the tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- move focus between the tree and buffer
+vim.api.nvim_set_keymap("n", "<A-h>", ":wincmd h<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-l>", ":wincmd l<CR>", { noremap = true, silent = true }) 
