fd1ab6afba1eb0a0722667b6e24c2b6be7b54d22
averyiaa <aaveryia@gmail.com>
2025-04-12 00:58:32 +0200

    remap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,8 @@
 -- spacebar leader key
 vim.g.mapleader = " "
 
--- keybind to toggle the tree
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-
 -- yank to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+
+-- keybind to toggle the tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
