71f4f2ded5982f128252d6c76e1f5f3f28dcb1e9
averyiaa <aaveryia@gmail.com>
2025-07-28 07:33:40 +0200

    indention

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,8 +3,8 @@ vim.o.relativenumber = true
 vim.o.swapfile = false
 vim.o.wrap = false
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.autoindent = true
