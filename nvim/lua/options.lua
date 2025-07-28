a455805b8469cbea54a02e8a845a114179c4e94a
averyiaa <aaveryia@gmail.com>
2025-07-28 07:56:18 +0200

    cleanup

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,6 +4,7 @@ vim.o.swapfile = false
 vim.o.wrap = false
 
 vim.o.tabstop = 2
+vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
@@ -11,6 +12,7 @@ vim.o.autoindent = true
 
 vim.o.smartcase = true
 vim.o.ignorecase = true
+vim.o.hlsearch = false
 vim.o.incsearch = true
 
 vim.o.termguicolors = true
