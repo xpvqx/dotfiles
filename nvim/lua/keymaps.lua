4bc40a1b9db6eb13ec8d198fcfa4f10c6a4aaef1
averyiaa <aaveryia@gmail.com>
2025-07-28 07:14:26 +0200

    ...

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,9 +4,13 @@ vim.g.mapleader = " "
 -- yank to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 
--- toggle the tree
+-- toggle nvim-tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- move focus between the tree and buffer
 vim.api.nvim_set_keymap("n", "<A-h>", ":wincmd h<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-l>", ":wincmd l<CR>", { noremap = true, silent = true }) 
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>o', ':quit<CR>:')
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,10 +1,16 @@
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.swapfile = false
+
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-vim.o.termguicolors = true
 vim.o.smartindent = true
-vim.o.swapfile = false
-vim.o.wrap = false
+vim.o.autoindent = true
+
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.incsearch = true
+
+vim.o.termguicolors = true
 vim.o.background = "dark"
