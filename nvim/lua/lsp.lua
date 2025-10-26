b58386800ad47ed5e3e5cbf319935ba0433e8c80
averyiaa <aaveryia@gmail.com>
2025-10-26 18:06:40 +0100

    fix python lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,22 +1,32 @@
--- lsp.lua
 require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "pyright" } })
 
-require("mason-lspconfig").setup({
-  ensure_installed = { "pyright" },
-})
-
-local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach = function(_, bufnr)
   local bufmap = function(mode, lhs, rhs)
-    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, { noremap = true, silent = true })
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true })
   end
-  bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  bufmap("n", "gd", vim.lsp.buf.definition)
+  bufmap("n", "K", vim.lsp.buf.hover)
 end
 
-lspconfig.pyright.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+-- Directly start Pyright when opening a Python file
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function(args)
+    vim.lsp.start({
+      name = "pyright",
+      cmd = { "pyright-langserver", "--stdio" },
+      filetypes = { "python" },
+      root_dir = vim.fs.dirname(
+        vim.fs.find(
+          { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git" },
+          { upward = true }
+        )[1] or vim.loop.cwd()
+      ),
+      capabilities = capabilities,
+      on_attach = on_attach,
+    }, { bufnr = args.buf })
+  end,
 })
